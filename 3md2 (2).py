57d9542319bb95607247be4e7cc960d99cd9ba7d
kak334 <76995455+kak334@users.noreply.github.com>
2021-01-07 10:28:48 +0200

    Add files via upload

---
+++ 3md2 (2).py
@@ -0,0 +1,17 @@
+from turtle import *
+
+     def square ():
+       right(90)
+          
+          
+     for i in range(10):
+        for j in range(4):
+            forward(25)
+            right(90)
+        penup()
+        forward(50)
+        pendown()
+    
+#ToRun
+speed(0)        
+square()  
